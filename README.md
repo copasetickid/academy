1fff3a25f4f73da9e29a42de89cb513309ef5ce9
Rushaine McBean <codekid24@gmail.com>
2013-09-16 08:18:23 -0400

    Initial commit/start of a new app

---
+++ README.md
@@ -0,0 +1,3 @@
+## Academy
+
+> This is e-commerce app for various dance  class lessons that will make you into a pop star.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Academy::Application.config.secret_key_base = 'e62063bb6a452573298c7c3c116301a0b7a9e195658ad9e1326a18c6ab1a336ade71a3ff2426e6990d87f43ba3b05768614009d5261166ffd104abdd180d50a1'
